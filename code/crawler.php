b332c918f2c8eecaea7f3a5cc7a89af95cf3e3e8
Kampol94 <37142187+Kampol94@users.noreply.github.com>
2018-09-13 00:57:29 +0200

    Update crawler.php

--- code/crawler.php
+++ code/crawler.php
@@ -1,7 +1,6 @@
 <?php
     // Constant
     const CRAWLER_DEPTH = 2;
-
 // -------------------------------------- OPEN DATABASE CONNECTION ---------------------------------------
     // Database access data (individual for each person)
     // $servername = "localhost";
@@ -12,17 +11,14 @@
     $username = "root";
     $password = "mysql";
     $dbname = "Crawler";
-
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } else {
         //echo "Connected successfully";
     }
-
 // ---------------------------------------------- FUNCTIONS ----------------------------------------------
     function crawlPage($url, $depth = 5){
         // Variables
@@ -32,49 +28,39 @@
             return;
         }
         $seen[$url] = true;
-
         // Create HTML object and get <a> tags
         $dom = new DOMDocument('1.0');
         @$dom->loadHTMLFile($url);
         $anchors = $dom->getElementsByTagName('a');
-
         // Extract urls from <a> tag
         foreach ($anchors as $element) {
             // Remove anchors
             $finalLink = explode("#", $element->getAttribute('href'));
             $link = $finalLink[0];
-
             // Add the protocol
             $adres = substr($link, 0, 7);
             $adresS = substr($link, 0, 8);
-
             $protocol = 'http://';
             $protocolS = 'https://';
-
             if($adres != $protocol && $adresS != $protocolS){
                 //echo '<br>Brak protokolu<br><br>';
                 $link = $url.$link;
             }
-
             array_push($result, $link);
         }
         $result = array_unique($result);
-
         return [
             'urls' => $result,
             'content' => $dom->saveHTML()
         ];
     }
-
 // ---------------------------------------------- MAIN CODE ----------------------------------------------
     // Variables
     $pageCrawlerResult = [];
-
     // Input (for url) validation
     if(!empty($_GET['url'])){
         $url = $_GET['url'];
     }
-
     // Run Crawler
     if(isset($url)) {
         if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
@@ -84,16 +70,13 @@
             $pageCrawlerResult = crawlPage($url, CRAWLER_DEPTH);
             $urls = $pageCrawlerResult['urls'];
             $pageContent = mysqli_real_escape_string($conn, $pageCrawlerResult['content']);
-
             // Save visited site (url + content)
             $sql = "INSERT INTO SitesViewed (site, content) VALUES ('$url', '$pageContent')";
-
             if ($conn->query($sql) === TRUE) {
                 //echo "New site crawled successfully","<br>";
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
             }
-
             // Save crawled urls into separate table
             foreach ($urls as $link){
                 $sql = "INSERT INTO SitesAwaiting (site) VALUES ('$link')";
@@ -105,7 +88,6 @@
             }
         }
     }
-
 // -------------------------------------- CLOSE DATABASE CONNECTION --------------------------------------
     // Close connection with database
     $conn->close();
